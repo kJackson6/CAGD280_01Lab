62c25e1fa2aba731988fc37e651e2882ba2f0a53
Katherine Jackson <129884531+kJackson6@users.noreply.github.com>
2023-10-31 13:26:06 -0700

    Changed sample scene and movement script
    Added cube
    -Rotated and moved
    Added cylinder
    -Rotated and moved
    Changed player movement code

--- VersionControlLab/Assets/Scripts/PlayerController.cs
+++ VersionControlLab/Assets/Scripts/PlayerController.cs
@@ -11,4 +11,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Impliment player movement
+    public float speed;
+    private Vector3 movePos;
+
+    private void MovePlayer()
+    {
+
+    }
+
+    private void Jump()
+    {
+        //makes the player jump
+    }
 }
